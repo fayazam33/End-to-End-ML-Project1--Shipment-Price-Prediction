72bdef57b372758fd38a0d0dda711efa9a9ad56a
Fayaz Ali Muktadir <121030621+fayazam33@users.noreply.github.com>
2026-06-18 00:13:44 +0600

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="fayaz_ali_muktadir",
+    author_email="456problemthinker@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
