9561e71c7d76dd67a293989cefe68bbb65eaad21
Fayaz Ali Muktadir <121030621+fayazam33@users.noreply.github.com>
2026-07-06 22:12:04 +0600

    adding the transformation process

--- shipment/pipline/training_pipeline.py
+++ shipment/pipline/training_pipeline.py
@@ -2,14 +2,16 @@ import sys
 from shipment.exception import shippingException
 from shipment.logger import logging
 from shipment.configuration.mongo_operation import MongoDBOperation
-from shipment.entity.artifact_entity import (DataIngestionArtifacts,DataValidationArtifacts)
+from shipment.entity.artifact_entity import (DataIngestionArtifacts,DataValidationArtifacts,
+                                             DataTransformationArtifacts)
 
-from shipment.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from shipment.entity.config_entity import (DataIngestionConfig,DataValidationConfig,
+                                           DataTransformationConfig)
 
 
 from shipment.components.data_ingestion import DataIngestion
 from shipment.components.data_validation import DataValidation
-# from shipment.components.data_transformation import DataTransformation
+from shipment.components.data_transformation import DataTransformation
 # from shipment.components.model_trainer import ModelTrainer
 
 
@@ -17,7 +19,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        # self.data_transformation_config = DataTransformationConfig()
+        self.data_transformation_config = DataTransformationConfig()
         # self.model_trainer_config = ModelTrainerConfig()
         self.mongo_op = MongoDBOperation()
 
@@ -64,27 +66,26 @@ class TrainPipeline:
     
 
     #  # This method is used to start the data transformation
-    # def start_data_transformation(
-    #     self, data_ingestion_artifact: DataIngestionArtifacts
-    # ) -> DataTransformationArtifacts:
-    #     logging.info(
-    #         "Entered the start_data_transformation method of TrainPipeline class"
-    #     )
-    #     try:
-    #         data_transformation = DataTransformation(
-    #             data_ingestion_artifacts=data_ingestion_artifact,
-    #             data_transformation_config=self.data_transformation_config,
-    #         )
-    #         data_transformation_artifact = (
-    #             data_transformation.initiate_data_transformation()
-    #         )
-    #         logging.info(
-    #             "Exited the start_data_transformation method of TrainPipeline class"
-    #         )
-    #         return data_transformation_artifact
+    def start_data_transformation(
+         self, data_ingestion_artifact: DataIngestionArtifacts) -> DataTransformationArtifacts:
+        logging.info(
+            "Entered the start_data_transformation method of TrainPipeline class"
+        )
+        try:
+            data_transformation = DataTransformation(
+                data_ingestion_artifacts=data_ingestion_artifact,
+                data_transformation_config=self.data_transformation_config,
+            )
+            data_transformation_artifact = (
+                data_transformation.initiate_data_transformation()
+            )
+            logging.info(
+                "Exited the start_data_transformation method of TrainPipeline class"
+            )
+            return data_transformation_artifact
 
-    #     except Exception as e:
-    #         raise shippingException(e, sys) from e
+        except Exception as e:
+            raise shippingException(e, sys) from e
         
     
 
@@ -115,9 +116,9 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
-            # data_transformation_artifact = self.start_data_transformation(
-            #     data_ingestion_artifact=data_ingestion_artifact
-            # )
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
             # model_trainer_artifact = self.start_model_trainer(
             #     data_transformation_artifact=data_transformation_artifact
             # )
--- shipment/utils/main_utils.py
+++ shipment/utils/main_utils.py
@@ -56,3 +56,17 @@ class MainUtils:
 
         except Exception as e:
             raise shippingException(e, sys) from e
+        
+    @staticmethod
+    def save_object(file_path: str, obj: object) -> None:
+        logging.info("Entered the save_object method of MainUtils class")
+        try:
+            with open(file_path, "wb") as file_obj:
+                dill.dump(obj, file_obj)
+
+            logging.info("Exited the save_object method of MainUtils class")
+
+            return file_path
+
+        except Exception as e:
+            raise shippingException(e, sys) from e
